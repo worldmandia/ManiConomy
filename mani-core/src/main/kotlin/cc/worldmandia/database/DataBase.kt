9b3fcd0171b127040cef7fd7baab2c81fff44488
mani123 <70480001+mani1232@users.noreply.github.com>
2023-07-06 15:58:37 +0200

    Add json type

--- mani-core/src/main/kotlin/cc/worldmandia/database/DataBase.kt
+++ mani-core/src/main/kotlin/cc/worldmandia/database/DataBase.kt
@@ -1,5 +1,6 @@
 package cc.worldmandia.database
 
+import cc.worldmandia.database.local.JsonDBAPI
 import cc.worldmandia.database.nosql.MongoDBAPI
 import cc.worldmandia.database.sql.MariaDBAPI
 
@@ -11,7 +12,7 @@ class DataBase<T>(var dbUrlOrPath: String, type: DataBaseType, tClass: Class<T>,
         dataBaseAPI = when (type) {
             DataBaseType.MONGO -> MongoDBAPI(this, tClass, dbName, dbCollection)
             DataBaseType.MARIADB -> MariaDBAPI(this, tClass, dbName, dbCollection)
-            else -> MongoDBAPI(this, tClass, dbName, dbCollection)
+            else -> JsonDBAPI(this, tClass, dbName, dbCollection)
         }
     }
 }
---
+++ mani-core/src/main/kotlin/cc/worldmandia/database/local/JsonDBAPI.kt
@@ -0,0 +1,32 @@
+package cc.worldmandia.database.local
+
+import cc.worldmandia.database.DataBase
+import cc.worldmandia.database.DataBaseAPI
+
+class JsonDBAPI<T>(
+    dataBase: DataBase<T>, tClass: Class<T>, dbName: String, dbCollection: String
+) : DataBaseAPI<T> {
+    override fun getObject(fieldId: String, fieldValue: Any): T? {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeObject(fieldId: String, fieldValue: Any): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllObjects(): Set<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(fieldId: String, fieldValue: Any): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun replaceObject(fieldId: String, fieldValue: Any, updateData: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun createObject(newObject: T): Boolean {
+        TODO("Not yet implemented")
+    }
+}
